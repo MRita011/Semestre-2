4eb0903378740033a83ec8f54e352d63bd821399
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-03-16 18:22:53 -0300

    Create AppLoja.java

---
+++ POO/2024-03-06-ArrayList/AppLoja.java
@@ -0,0 +1,11 @@
+public class AppLoja {
+    private Loja lojinha;
+
+    public AppLoja() {
+        lojinha = new Loja();
+    }
+
+    public void executar() {
+        System.out.println("Ayooo");
+    }
+}
