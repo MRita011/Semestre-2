92561fe7ca7aecc2bb2661f8b457fa8fa6cb9a5f
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-03-16 18:27:09 -0300

    Create Main.java

---
+++ POO/2024-03-10-AtividadeDuplas/Main.java
@@ -0,0 +1,9 @@
+// [16] Remover todos os produtos até um determinado ano
+//  Maria Rita Rodrigues e Eduarda Sobreiro
+
+public class Main {
+    public static void main(String[] args) {
+        AppLoja appLoja = new AppLoja();
+        appLoja.executar();
+    }
+}
