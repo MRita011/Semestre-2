fd25d11765b92106f79e72e123e5f516c762b418
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-03-30 22:29:00 -0300

    Update Plantel.java

--- POO/Exercicivios-Avaliativos/E1/Plantel.java
+++ POO/Exercicivios-Avaliativos/E1/Plantel.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
-public class Plantel 
+public class Plantel
 {
-	private ArrayList<Atleta> atletas;
+	private final ArrayList<Atleta> atletas;
 
 	public Plantel() {
-		atletas = new ArrayList<Atleta>();
+		atletas = new ArrayList<>();
 	}
 
 	public ArrayList<Atleta> getAtletas() {
@@ -17,33 +17,39 @@ public class Plantel
 	// retorna true se o cadastro teve sucesso; ou false em caso contrário.
 
 	public boolean cadastraAtleta(Atleta a) {
-		for (Atleta atleta : atletas) {
-			if (a.getNumero() == atleta.getNumero()) {
-				return false; // Já existe um atleta com o mesmo número
-			}
+		if(consultaAtleta(a.getNumero()) == null) {
+			atletas.add(a);
+			return true;
 		}
-		atletas.add(a);
-		return true; // Cadastro bem-sucedido
+		return false;
 	}
 
 	// retorna o atleta com o nome indicado.
 	// Se não houver nenhum atleta com este nome retorna null.
 
 	public Atleta consultaAtleta(String nome) {
-		for (Atleta a : atletas) {
-			if (a.getNome().equalsIgnoreCase(nome))
-				return a;
+		if (!atletas.isEmpty()) {
+			for (int i = 0; i < atletas.size(); i++) {
+				Atleta a = atletas.get(i);
+
+				if (a.getNome().equalsIgnoreCase(nome))
+					return a;
+			}
 		}
-		return null; // Retorna null se o atleta não for encontrado
+		return null;
 	}
 
 	// retorna o atleta com o número indicado.
 	// Se não houver nenhum atleta com este número retorna null.
 
 	public Atleta consultaAtleta(int numero) {
-		for (Atleta a : atletas) {
-			if (a.getNumero() == numero)
-				return a;
+		if(!atletas.isEmpty()) {
+			for (int i = 0; i < atletas.size(); i++) {
+				Atleta a = atletas.get(i);
+
+				if (a.getNumero() == numero)
+					return a;
+			}
 		}
 		return null;
 	}
