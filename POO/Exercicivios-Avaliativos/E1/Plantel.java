3fd4034a2607bd9ea8a3153d79cd411e0455b592
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-04-02 10:49:39 -0300

    Update Plantel.java

--- POO/Exercicivios-Avaliativos/E1/Plantel.java
+++ POO/Exercicivios-Avaliativos/E1/Plantel.java
@@ -2,55 +2,52 @@ import java.util.ArrayList;
 
 public class Plantel
 {
-	private final ArrayList<Atleta> atletas;
+    private ArrayList<Atleta> atletas;
 
-	public Plantel() {
-		atletas = new ArrayList<>();
-	}
+    public Plantel() {
+        atletas = new ArrayList<>();
+    }
 
-	public ArrayList<Atleta> getAtletas() {
-		return atletas;
-	}
+    public ArrayList<Atleta> getAtletas() {
+        return atletas;
+    }
+    // recebe como parâmetro um novo Atleta e o cadastra no sistema.
+    // não pode haver atletas com o mesmo número.
+    // retorna true se o cadastro teve sucesso; ou false em caso contrário.
 
-	// recebe como parâmetro um novo Atleta e o cadastra no sistema.
-	// não pode haver atletas com o mesmo número.
-	// retorna true se o cadastro teve sucesso; ou false em caso contrário.
+    public boolean cadastraAtleta(Atleta atleta) {
+        if (consultaAtleta(atleta.getNumero()) == null) {
+            atletas.add(atleta);
+            return true;
+        }
+        return false;
+    }
 
-	public boolean cadastraAtleta(Atleta a) {
-		if(consultaAtleta(a.getNumero()) == null) {
-			atletas.add(a);
-			return true;
-		}
-		return false;
-	}
+    // retorna o atleta com o nome indicado.
+    // Se não houver nenhum atleta com este nome retorna null
 
-	// retorna o atleta com o nome indicado.
-	// Se não houver nenhum atleta com este nome retorna null.
+    public Atleta consultaAtleta(String nome) {
+        if (!atletas.isEmpty()) {
+            for (Atleta atleta : atletas) {
+                if (atleta.getNome().equals(nome))
+                    return atleta;
+            }
+        }
+        return null;
+    }
 
-	public Atleta consultaAtleta(String nome) {
-		if (!atletas.isEmpty()) {
-			for (int i = 0; i < atletas.size(); i++) {
-				Atleta a = atletas.get(i);
+    // retorna o atleta com o número indicado.
+    // Se não houver nenhum atleta com este número retorna null.
 
-				if (a.getNome().equalsIgnoreCase(nome))
-					return a;
-			}
-		}
-		return null;
-	}
+    public Atleta consultaAtleta(int numero) {
+        if (!atletas.isEmpty()) {
+            for (Atleta atleta : atletas) {
+                if (atleta.getNumero() == numero)
+                    return atleta;
+            }
+        }
+        return null;
+    }
 
-	// retorna o atleta com o número indicado.
-	// Se não houver nenhum atleta com este número retorna null.
 
-	public Atleta consultaAtleta(int numero) {
-		if(!atletas.isEmpty()) {
-			for (int i = 0; i < atletas.size(); i++) {
-				Atleta a = atletas.get(i);
-
-				if (a.getNumero() == numero)
-					return a;
-			}
-		}
-		return null;
-	}
 }
