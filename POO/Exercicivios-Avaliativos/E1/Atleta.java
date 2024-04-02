03307aa64e7b221b9af51d84fbb04779230cf2fd
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-04-02 10:48:57 -0300

    Update Atleta.java

--- POO/Exercicivios-Avaliativos/E1/Atleta.java
+++ POO/Exercicivios-Avaliativos/E1/Atleta.java
@@ -1,57 +1,50 @@
-// classe que representa um atleta da competição:
 import java.util.ArrayList;
 
 public class Atleta
 {
-	private int numero;
-	private String nome;
-	private String pais;
-	private int quant;
+    private int numero;
+    private String nome;
+    private String pais;
+    private ArrayList<Medalha> medalhas;
 
-	private ArrayList<Medalha> medalhas;
 
-	public Atleta(int numero, String nome, String pais) {
-		this.numero = numero;
-		this.nome = nome;
-		this.pais = pais;
-		this.medalhas = new ArrayList<>();
-		quant = 0;
-	}
+    public Atleta(int numero, String nome, String pais) {
+        this.numero = numero;
+        this.nome = nome;
+        this.pais = pais;
+        this.medalhas = new ArrayList<>();
+    }
 
-	public ArrayList<Medalha> getMedalhas() {
-		return medalhas;
-	}
+    public int getNumero() {
+        return numero;
+    }
 
-	public int getNumero() {
-		return numero;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getPais() {
+        return pais;
+    }
 
-	public String getPais() {
-		return pais;
-	}
-	// adiciona uma nova medalha ao atleta
+    public ArrayList<Medalha> getMedalhas() {
+        return medalhas;
+    }
 
-	public void adicionaMedalha(Medalha m) {
-		medalhas.add(m);
-	}
+    public void adicionaMedalha(Medalha medalha) {
+        medalhas.add(medalha);
+    }
 
-//	retorna a quantidade de medalhas do atleta
-	public int consultaQuantidadeMedalha() {
-		quant = medalhas.size();
-		return quant;
-	}
+    public int consultaQuantidadeMedalhas(int tipo) {
+        int quantidade = 0;
 
-	public int quantidadeMedalhasTipo(int tipo) {
-		int quantidade = 0;
+        for (Medalha medalha : medalhas) {
+            if (medalha.getTipo() == tipo)
+                quantidade++;
+        }
 
-		for (Medalha medalha : medalhas) {
-			if (medalha.getTipo() == tipo)
-				quantidade++;
-		}
-		return quantidade;
-	}
+        return quantidade;
+    }
+
 }
+
