9c3bdbcc18de599490a711e4b2e77590d76da48c
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-04-02 10:49:26 -0300

    Update Medalheiro.java

--- POO/Exercicivios-Avaliativos/E1/Medalheiro.java
+++ POO/Exercicivios-Avaliativos/E1/Medalheiro.java
@@ -2,57 +2,60 @@ import java.util.ArrayList;
 
 public class Medalheiro
 {
-	private final ArrayList<Medalha> medalhas;
+    private ArrayList <Medalha> medalhas;
 
-	public Medalheiro() {
-		medalhas = new ArrayList<>();
-	}
+    public Medalheiro() {
+        medalhas = new ArrayList<>();
+    }
 
-	public ArrayList<Medalha> getMedalhas() {
-		return medalhas;
-	}
+    public ArrayList<Medalha> getMedalhas() {
+        return medalhas;
+    }
 
-	//  Recebe como parâmetro uma nova medalha e a cadastra no sistema.
-	// 	Não pode haver medalhas com o mesmo código.
-	// 	Retorna true se o cadastro teve sucesso; ou false em caso contrário
+    //  Recebe como parâmetro uma nova medalha e a cadastra no sistema.
+    // 	Não pode haver medalhas com o mesmo código.
+    // 	Retorna true se o cadastro teve sucesso; ou false em caso contrário
 
-	public boolean cadastraMedalha(Medalha m) {
-		if (consultaMedalha(m.getCodigo()) != null)
-			return false;
+    public boolean cadastraMedalha(Medalha medalha) {
+        if (consultaMedalha(medalha.getCodigo()) != null) {
+            return false;
+        }
 
-		medalhas.add(m);
-		return true;
-	}
+        medalhas.add(medalha);
+        return true;
+    }
 
-	//	Retorna a medalha com o código indicado.
-	// 	Se não houver medalha com este código retorna null.
+    //	Retorna a medalha com o código indicado.
+    // 	Se não houver medalha com este código retorna null.
 
-	public Medalha consultaMedalha(int codigo) {
-		if(!medalhas.isEmpty()) {
-			for (Medalha m : medalhas) {
-				if (m.getCodigo() == codigo)
-					return m;
-			}
-		}
-		return null;
-	}
-
-	//	Retorna uma coleção de medalhas com a modalidade indicada.
-	// 	Se não houver nenhuma medalha com esta modalidade retorna null.
-
-	public ArrayList<Medalha> consultaMedalhas(String modalidade) {
-		ArrayList<Medalha> medalhasEncontradas = new ArrayList<>();
-
-		if (!medalhas.isEmpty()) {
-            for (Medalha m : medalhas) {
-                if (m.getModalidade().equalsIgnoreCase(modalidade))
-                    medalhasEncontradas.add(m);
+    public Medalha consultaMedalha(int codigo) {
+        if (!medalhas.isEmpty()) {
+            for (Medalha medalha : medalhas) {
+                if(medalha.getCodigo() == codigo)
+                    return medalha;
             }
-		}
+        }
+        return null;
+    }
 
-		if (medalhasEncontradas.isEmpty())
-			return null;
+    //	Retorna uma coleção de medalhas com a modalidade indicada.
+    // 	Se não houver nenhuma medalha com esta modalidade retorna null.
 
-		return medalhasEncontradas;
-	}
+    public ArrayList<Medalha> consultaMedalhas(String modalidade) {
+        ArrayList<Medalha> medalhasEncontradas = new ArrayList<>();
+
+        if (!medalhas.isEmpty()) {
+            for (Medalha medalha : medalhas) {
+                if (medalha.getModalidade().equalsIgnoreCase(modalidade)) {
+                    medalhasEncontradas.add(medalha);
+                }
+            }
+        }
+
+        if (medalhasEncontradas.isEmpty()) {
+            return null;
+        }
+        
+        return medalhasEncontradas;
+    }
 }
