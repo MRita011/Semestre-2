b13aec4618e4f1d2684ce35fad2a0b145fa773e7
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-04-07 17:23:08 -0300

    Create Pedido.java

---
+++ POO/2024-04-04-ExemploPacotes/dados/Pedido.java
@@ -0,0 +1,14 @@
+package dados;
+
+public class Pedidos
+{
+    private double valor;
+
+    public Pedidos(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
