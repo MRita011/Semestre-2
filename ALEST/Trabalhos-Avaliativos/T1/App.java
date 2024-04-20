4119f5391c40a85c478dd19e21d09bc39819e654
Maria Rita Rodrigues <142165997+MRita011@users.noreply.github.com>
2024-04-20 00:43:57 -0300

    Update App.java

--- ALEST/Trabalhos-Avaliativos/T1/App.java
+++ ALEST/Trabalhos-Avaliativos/T1/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println(
                 "/**************************************************\n" +
-                "/** NOMES | Alexya Ungaratti e Maria Rita Rodrigues \n" +
-                "/**************************************************\n"
+                        "/** NOMES | Alexya Ungaratti e Maria Rita Rodrigues \n" +
+                        "/**************************************************\n"
         );
 
         DoubleLinkedListOfInteger dlist = new DoubleLinkedListOfInteger();
@@ -53,45 +53,45 @@ public class App {
         System.out.println("\n/***************************************\n" + "/** T1 | PARTE 2: Implementação da pilha\n" + "/***************************************\n");
 
         // Instanciando a pilha
-        StackOfInteger stack = new StackOfInteger();
+        StackOfInteger pilha = new StackOfInteger();
 
         // Inserindo elementos na pilha (pegou a referência?! (๑˃ᴗ˂)ﻭ)
-        stack.push(0);
-        stack.push(10);
-        stack.push(20);
-        stack.push(35);
-        stack.push(44);
-        stack.push(40);
-        stack.push(60);
-        stack.push(70);
-        stack.push(80);
-        stack.push(90);
-        stack.push(10);
+        pilha.push(0);
+        pilha.push(10);
+        pilha.push(20);
+        pilha.push(35);
+        pilha.push(44);
+        pilha.push(40);
+        pilha.push(60);
+        pilha.push(70);
+        pilha.push(80);
+        pilha.push(90);
+        pilha.push(10);
 
         // Mostrando todos os elementos da pilha
-        System.out.println("Elementos da pilha: " + stack.stackToString(stack));
-        System.out.println("Tamanho da pilha: " + stack.size());
+        System.out.println("Elementos da pilha: " + pilha.stackToString(pilha));
+        System.out.println("Tamanho da pilha: " + pilha.size());
 
         // Mostrando o topo da pilha
-        System.out.println("Top: " + stack.top());
+        System.out.println("Top: " + pilha.top());
 
         // Removendo um elemento da pilha
-        System.out.println("Pop: " + stack.pop());
+        System.out.println("Pop: " + pilha.pop());
 
         // Verificando o tamanho da pilha
-        System.out.println("Size: " + stack.size());
+        System.out.println("Size: " + pilha.size());
 
         // Verificando se a pilha está vazia
-        System.out.println("\nA pilha está vazia? " + stack.isEmpty());
+        System.out.println("\nA pilha está vazia? " + pilha.isEmpty());
 
         // Limpando a pilha
-        stack.clear();
+        pilha.clear();
 
         // Verificando se a pilha está vazia após o clear
-        System.out.println("A pilha está vazia após limpar? " + stack.isEmpty());
+        System.out.println("A pilha está vazia após limpar? " + pilha.isEmpty());
 
         // Mostrando a pilha após o clear
-        System.out.println("Elementos da pilha: " + stack.stackToString(stack));
+        System.out.println("Elementos da pilha: " + pilha.stackToString(pilha));
 
 
         System.out.println("\n/******************************************\n" + "/** T1 | PARTE 2.1: Implementação da pilha\n" + "/******************************************\n");
@@ -99,11 +99,67 @@ public class App {
         // Testando o método reverseArrayUsingStack()
         int[] array = {1, 2, 3, 4, 5};
         System.out.print("Array  original: ");
-        for (int num : array) { System.out.print(num + " | "); }
+        for (int num : array) {
+            System.out.print(num + " | ");
+        }
 
         StackOfInteger.reverseArrayUsingStack(array);
 
         System.out.print("\nArray invertido: ");
-        for (int num : array) { System.out.print(num + " | "); }
+        for (int num : array) {
+            System.out.print(num + " | ");
+        }
+
+
+        System.out.println("\n\n/***************************************\n" + "/** T1 | PARTE 3: Implementação da fila\n" + "/***************************************\n");
+
+        // Instanciando a fila
+        QueueOfInterger fila = new QueueOfInterger();
+
+        // Adicionando elementos à fila
+        fila.enqueue(10);
+        fila.enqueue(20);
+        fila.enqueue(35);
+        fila.enqueue(44);
+        fila.enqueue(40);
+        fila.enqueue(60);
+        fila.enqueue(70);
+        fila.enqueue(80);
+        fila.enqueue(90);
+        fila.enqueue(11);
+        fila.enqueue(77);
+
+        // Elementos da fila
+        System.out.println("Elementos da fila: " + fila.queueToString(fila));
+
+        // Imprimindo o tamanho da fila
+        System.out.println("Tamanho da fila: " + fila.size());
+
+        // Imprimindo o elemento na frente da fila
+        System.out.println("Head: " + fila.head());
+
+        // Removendo elementos da fila
+        System.out.println("\nDequeue: " + fila.dequeue());
+        System.out.println("Dequeue: " + fila.dequeue());
+        System.out.println("Dequeue: " + fila.dequeue());
+        System.out.println("Dequeue: " + fila.dequeue());
+
+        System.out.println("\nElementos da fila: " + fila.queueToString(fila));
+        System.out.println("Tamanho da fila: " + fila.size());
+
+        // Verificando se a fila está vazia após a remoção
+        System.out.println("\nA fila está vazia? " + fila.isEmpty());
+
+//        // Testando o método enqueuePriority() com a lista atual como base para prioridades
+//        int size = fila.size();
+//        for (int i = 0; i < size; i++) {
+//            Integer element = fila.dequeue();
+//            fila.enqueuePriority(element);
+//        }
+//
+//
+//        // Imprimindo a fila de prioridade
+//        System.out.println("Elementos da fila de prioridade: " + fila.queueToString(fila));
+//        System.out.println("Tamanho da fila de prioridade: " + fila.size());
     }
 }
